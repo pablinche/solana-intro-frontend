1dc4b4fc8548bfd8fbed90e528894da62d564170
pablinche <6430577+pablinche@users.noreply.github.com>
2022-12-04 08:50:07 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,39 @@ import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
 import * as Web3 from '@solana/web3.js'
+import { connect } from 'http2'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [owner, setOwner] = useState<any | null>('')
   const [isExecutable, setIsExecutable] = useState(false)
+  const [rentEpoch, setRentEpoch] = useState<any | null>('Waiting')
 
   const addressSubmittedHandler = (address: string) => {
     try {
       setAddress(address)
       const key = new Web3.PublicKey(address)
       const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+      
       connection.getBalance(key).then(balance => {
         setBalance(balance / Web3.LAMPORTS_PER_SOL)
       })
+
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false);
+      })
+
+      connection.getAccountInfo(key).then(info => {
+        setOwner(info?.owner)
+        console.log(info?.owner)
+      })
+
+      connection.getAccountInfo(key).then(info => {
+        setRentEpoch(info?.rentEpoch)
+        console.log(info?.rentEpoch)
+      })
+      
     } catch (error) {
       setAddress('')
       setBalance(0)
@@ -31,9 +50,11 @@ const Home: NextPage = () => {
           Start Your Solana Journey
         </p>
         <AddressForm handler={addressSubmittedHandler} />
-        <p>{`Address: ${address}`}</p>
-        <p>{`Balance: ${balance} SOL`}</p>
-        <p>{`Is Executable: ${isExecutable}`}</p>
+        <p>{`Address: ${address}`}<br />
+           {`Balance: ${balance} SOL`}<br />
+           {`Owner: ${owner}`}<br />
+           {`Rent Epoch: ${rentEpoch}`}<br />
+           {`Is Executable: ${isExecutable ? 'Yep' : 'Nope'}`}</p>
       </header>
     </div>
   )
